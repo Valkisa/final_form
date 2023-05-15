2478f113883b610d95b217eeb918f595d123eee4
Valkisa <116564646+Valkisa@users.noreply.github.com>
2023-05-15 23:59:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function openNav() {
+    document.getElementById("sideNav").classList.add("open");
+}
+
+function closeNav() {
+    document.getElementById("sideNav").classList.remove("open");
+}
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+smallimg[0].onclick = function () {
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function () {
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function () {
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function () {
+    MainImg.src = smallimg[3].src;
+}
